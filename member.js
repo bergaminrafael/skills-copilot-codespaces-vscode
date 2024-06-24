9caf4369980a393c7aac41d3c0c245cb354ed464
Rafael Bergamin <bergaminrafael@gmail.com>
2024-06-24 17:44:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  const skills = {
+    html: 5,
+    css: 5,
+    js: 5,
+    react: 5,
+    node: 5,
+    mongo: 4,
+    express: 4,
+    git: 4,
+    bash: 3,
+    aws: 3,
+    docker: 3,
+  };
+  return skills;
+}
